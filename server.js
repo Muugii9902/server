972a403eb49672e70c15b8f8e5b4edba0a11c383
Muugii9902 <muugii9902@gmail.com>
2024-08-20 12:49:59 +0800

    server

--- server.js
+++ server.js
@@ -15,14 +15,28 @@ app.post("/users", (req, res) => {
   users.push(newUser);
   res.status(201).json({ user: newUser });
 });
-app.put("/users/userId", (req, res) => {
+app.put("/users/:userId", (req, res) => {
   const findIndex = users.findIndex(
-    (user) => user.id === parseInt(req, params.userId)
+    (user) => user.id === parseInt(req.params.userId)
   );
-  res.status("put request is successfully");
+  if (findIndex > -1) {
+    users[findIndex].name = req.body.name;
+    res.status(200).json({ user: users[findIndex] });
+  } else {
+    res.status(400).json({ message: "no" });
+  }
 });
-app.delete("/", (req, res) => {
-  res.send("delete request is successfully");
+app.delete("/users/:id", (req, res) => {
+  const findIndex = users.findIndex(
+    (user) => user.id === parseInt(req.params.id)
+  );
+  console.log("id", req.params.id);
+  if (findIndex > -1) {
+    const deletedUser = users.splice(findIndex, 1);
+    res.status(200).json({ user: deletedUser[0] });
+  } else {
+    res.status(400).json({ message: "not found " });
+  }
 });
 app.listen(8000, () => {
   console.log("server is running at localhost:8000");
