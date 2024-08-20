31fe7353a7fb8bc838ff9f9f651c508ef91181b0
Muugii9902 <muugii9902@gmail.com>
2024-08-20 20:07:24 +0800

    server aldaa

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express = require("express");
+const fs =require("fs");
+
 const app = express();
 app.use(express.json());
-const users = [{ id: 1, name: "naraa", age: 20 }];
+
 
 app.get("/users", (req, res) => {
-  res.status(200).json({ users: users });
+  const data = fs.readFileSync("/users.json",{encoding:'utf8'});
+  const obData = JSON.parse(data)
+  res.status(200).json({ users: obData.users });
 });
 app.post("/users", (req, res) => {
+  const data = fs.readFileSync("./users.json",{encoding:'utf8'});
+  const {users} = JSON.parse(data);
   const newUser = {
-    id: users.length + 1,
+    id: `${users.length + 1}`,
     name: req.body.name,
     age: req.body.age,
   };
   users.push(newUser);
+  fs.writeFileSync(".users.json",JSON.stringify({users}));
   res.status(201).json({ user: newUser });
 });
 app.put("/users/:userId", (req, res) => {
