acbe13c6546e80c1d78a2f58b662ed16d645399f
Muugii9902 <muugii9902@gmail.com>
2024-08-21 17:03:07 +0800

    good

--- server.js
+++ server.js
@@ -19,19 +19,19 @@ app.post("/users", (req, res) => {
     age: req.body.age,
   };
   users.push(newUser);
-  fs.writeFileSync(".users.json",JSON.stringify({users}));
+  fs.writeFileSync("users.json",JSON.stringify({users}));
   res.status(201).json({ user: newUser });
 });
 app.put("/users/:userId", (req, res) => {
   const data = fs.readFileSync("./users.json",{encoding:'utf8'});
   const {users} = JSON.parse(data);
   const findIndex = users.findIndex(
-    (user) => user.id === parseInt(req.params.userId)
+    (user) => user.id === req.params.userId
   );
   if (findIndex > -1) {
     users[findIndex].name = req.body.name;
-    fs.writeFileSync(".users.json",JSON.stringify({users}));
-    res.status(200).json({ user: users[findIndex],data:"" });
+    fs.writeFileSync("users.json",JSON.stringify({users}));
+    res.status(200).json({ user: users[findIndex], });
   } else {
     res.status(400).json({ message: "no" });
   }
@@ -40,13 +40,13 @@ app.delete("/users/:id", (req, res) => {
   const data = fs.readFileSync("./users.json",{encoding:'utf8'});
   const {users} = JSON.parse(data);
   const findIndex = users.findIndex(
-    (user) => user.id === parseInt(req.params.id)
+    (user) => user.id === req.params.id
   );
   console.log("id", req.params.id);
   if (findIndex > -1) {
     const deletedUser = users.splice(findIndex, 1);
-    fs.writeFileSync(".users.json",JSON.stringify({users}));
-    res.status(200).json({ user: deletedUser[0] });
+    fs.writeFileSync("users.json",JSON.stringify({users}));
+    res.status(200).json({ user: deletedUser[0], });
   } else {
     res.status(400).json({ message: "not found " });
   }
